cbca1a44dcb23451b44bd88d976b6aa521c33a24
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-02-03 18:36:28 -0400

    Modelo Creado.

---
+++ Darvyn_Lavandier_P1_AP1/Models/Registros.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DarvynLuisLavandier_AP1_P1.Models
+{
+    public class Registros
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string? Nombres { get; set; }
+
+
+    }
+}
