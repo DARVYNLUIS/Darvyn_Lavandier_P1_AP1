3d10abd7ba6ebe2218ba6aa8ceba0afdc53fdd1f
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-02-03 18:37:13 -0400

    Contexto Creado.

---
+++ Darvyn_Lavandier_P1_AP1/DAL/Contexto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DarvynLuisLavandier_AP1_P1.Models;
+
+
+namespace RegistroTecnico.DAL
+{
+   
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Registros> Tecnicos { get; set; }
+
+
+
+    }
+
+}
