873a8c9f413ed860a8300858350bcfa8a11499d3
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-02-17 08:39:34 -0400

    Service creado.

---
+++ Darvyn_Lavandier_P1_AP1/Services/RegistroService.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq.Expressions;
+using Darvyn_Lavandier_P1_AP1.DAL;
+using Microsoft.EntityFrameworkCore;
+namespace Darvyn_Lavandier_P1_AP1.Services
+{
+    public class Servicio(IDbContextFactory<Contexto> DbFactory)
+    {
+       
+        public async Task<bool> Guardar()
+        {
+            return true;
+        }
+  
+        public async Task<bool> Existe()
+        {
+            return true;
+        }
+  
+        public async Task<bool> Insertar()
+        {
+            return false;
+        }
+
+        public async Task<bool> Modificar()
+        {
+            return true;
+        }
+     
+        public async Task<bool> Buscar(int id)
+        {
+
+            return true;
+        }
+
+        public async Task<bool> Eliminar()
+        {
+            return true;
+        }
+
+        public async Task<bool> Listar()
+        {
+            return true;
+        }
+    }
+}
