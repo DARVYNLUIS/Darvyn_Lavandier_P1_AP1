9aa60f51a66582411c6bd33704be5eed6a2d9fc4
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-02-17 08:34:34 -0400

    Modelo creado.

---
+++ Darvyn_Lavandier_P1_AP1/Models/Registros.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Darvyn_Lavandier_P1_AP1.Models
+{
+    public class Registros
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+    }
+}
