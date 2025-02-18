1db8c5626d441833cd9ae7fba314c256802b5053
Darvyn Luis Lavandier Quezada <darvyn_lavandier@ucne.edu.do>
2025-02-17 20:21:24 -0400

    Modelo creado.

--- Darvyn_Lavandier_P1_AP1/Models/Registros.cs
+++ Darvyn_Lavandier_P1_AP1/Models/Registros.cs
@@ -6,14 +6,17 @@ namespace Darvyn_Lavandier_P1_AP1.Models
         [Key]
         public int Id { get; set; }
 
-        public string Persona { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string? Persona { get; set; }
 
-        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "La observacion es obligatorio")]
+        public string? Observacion { get; set; }
 
-        public string observacion { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
 
-        public int monto {  get; set; } 
-        
-        
+
+          [Required(ErrorMessage = "El monto es obligatorio")]
+        public int? Monto { get; set; }
+
     }
 }
